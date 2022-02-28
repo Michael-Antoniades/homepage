b0f08e6158bd07241627270aa0445a3f830f18d9
Michael Antoniades <scmikeyt@yahoo.com>
2022-02-28 13:51:22 -0500

    Updates

---
+++ precache-manifest.78cad9ee80d70c913c18d34b32cb4047.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6a1cbe775537ea9d642121ddc8bdb0",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "d48a7cae3a7d7c5fe368",
+    "url": "/homepage/static/css/main.18e9883a.chunk.css"
+  },
+  {
+    "revision": "d3ebb72829c6fe242b56",
+    "url": "/homepage/static/js/2.b296ad6b.chunk.js"
+  },
+  {
+    "revision": "669a6aa274b3cde4d24b713448784ce3",
+    "url": "/homepage/static/js/2.b296ad6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d48a7cae3a7d7c5fe368",
+    "url": "/homepage/static/js/main.f20640dc.chunk.js"
+  },
+  {
+    "revision": "836f55bc99e549bdd710",
+    "url": "/homepage/static/js/runtime-main.3ed86e83.js"
+  },
+  {
+    "revision": "cdbfe560083a798f6e44d28c04841663",
+    "url": "/homepage/static/media/background_image.cdbfe560.jpg"
+  }
+]);
